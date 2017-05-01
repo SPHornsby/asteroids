c12f92f609f16650b56ffe9dd5487094bfa37c28
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-01 16:07:44 -0700

    Template

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+window.onload = function() {
+
+  const game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
+
+
+  function preload () {
+  }
+  
+  function create () {
+  }
+
+  function update () {
+  }
+};
